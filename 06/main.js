378591d57ee8870d8ed8edda0295564b9e4e67b3
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-06 08:25:12 +0100

    day 4

---
+++ 06/main.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+let fileContent = fs.readFileSync('data.csv', 'utf8');
+let cleanedFileContent = fileContent
+  .replaceAll('\n\n', '_')
+  .replaceAll('\n', ' ');
+const rowsPart1 = cleanedFileContent.replaceAll(' ', '').split('_');
+const rowsPart2 = cleanedFileContent.split('_');
+
+const getNumUniqueAnswers = (groupAnswers) => {
+  return new Set(Array.from(groupAnswers)).size;
+};
+
+const getSharedAnswers = (groupAnswers) => {
+  const individualAnswers = groupAnswers.split(' ').map((x) => Array.from(x));
+  const sharedAnswers = individualAnswers.reduce((a, b) => {
+    return a.filter((x) => b.includes(x));
+  }, individualAnswers[0]);
+  return new Set(sharedAnswers).size;
+};
+
+const sum = (a, b) => a + b;
+
+const part1Answer = rowsPart1.map(getNumUniqueAnswers).reduce(sum, 0);
+const part2Answer = rowsPart2.map(getSharedAnswers).reduce(sum, 0);
+
+console.log(part1Answer);
+console.log(part2Answer);
