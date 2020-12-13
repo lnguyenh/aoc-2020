dfe8526fb6a57bd30e526e9b6ca002229f7436e5
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-13 17:30:58 +0100

    day 13 part 2 slow (too slow)

--- 13/main.js
+++ 13/main.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
+const mathjs = require('mathjs');
 
 class Bus {
-  constructor(id) {
+  constructor(id, index) {
     this.id = id;
+    this.period = id;
+    this.index = index;
+    this.delta = index;
   }
 
   getNextDeparture(time) {
     return Math.ceil(time / this.id) * this.id;
+  }
+
+  hasDeparture(time, delta) {
+    const targetTime = time - delta + this.index;
+    const result = targetTime % this.id === 0;
+    if (result) console.log(time, delta, this.period);
+    return result;
+  }
+}
+function* busDeparture(bus) {
+  let index = 0;
+  while (true) {
+    index++;
+    yield bus.id * index;
   }
 }
 
@@ -14,9 +32,9 @@ const getInput = (fileName) => {
   let fileContent = fs.readFileSync(fileName, 'utf8');
   const inputAsText = fileContent.split('\n');
   const Buses = [];
-  for (const id of inputAsText[1].split(',')) {
+  for (const [index, id] of inputAsText[1].split(',').entries()) {
     if (isNaN(id)) continue;
-    Buses.push(new Bus(Number(id)));
+    Buses.push(new Bus(Number(id), index));
   }
   return {
     target: Number(inputAsText[0]),
@@ -33,7 +51,31 @@ const getBestNextDeparture = (target, buses) => {
   return { bestDeparture, bus, waitTime, result };
 };
 
-const INPUT_FILE = 'data.csv';
+const INPUT_FILE = 'example.csv';
 const { target, buses } = getInput(INPUT_FILE);
+
 const { result } = getBestNextDeparture(target, buses);
 console.log('part 1: ' + result);
+
+sortedBuses = buses.sort((a, b) => b.id - a.id);
+console.log(sortedBuses);
+const slowestBus = sortedBuses[0];
+const otherBuses = sortedBuses.slice(1);
+const slowestBusDepartures = busDeparture(slowestBus);
+
+// part 2 slow
+let targetTime;
+while (true) {
+  targetTime = slowestBusDepartures.next().value;
+  // console.log(targetTime);
+  if (
+    otherBuses.every((bus) => {
+      return bus.hasDeparture(targetTime, slowestBus.index);
+    })
+  )
+    break;
+}
+console.log(targetTime - slowestBus.index);
+
+// const lcm = mathjs.lcm(23, 41, 37, 479, 13, 17, 29, 373, 19);
+// console.log(lcm);
