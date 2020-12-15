64059a7403be949db9810396aa8b006aa09ea6ad
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-15 08:24:53 +0100

    Day 15 part 1

--- 15/main.js
+++ 15/main.js
@@ -1,10 +1,50 @@
-const fs = require('fs');
+class Game {
+  constructor(seed) {
+    this.turn = 0;
+    this.turns = [];
+    this.lastLastSpoken = {};
+    this.lastSpoken = {};
+    for (const value of seed) {
+      this.applyTurn(value);
+    }
+    this.mostRecentlySpoken = this.turns.slice(-1)[0];
+  }
 
-const getInput = (fileName) => {
-  let fileContent = fs.readFileSync(fileName, 'utf8');
-  const inputAsText = fileContent.split('\n');
-  return inputAsText;
-};
+  getAge(value) {
+    return value in this.lastLastSpoken
+      ? this.lastSpoken[value] - this.lastLastSpoken[value]
+      : 0;
+  }
 
-const INPUT_FILE = 'example.csv';
-const input = getInput(INPUT_FILE);
+  applyTurn(turnValue) {
+    if (turnValue in this.lastSpoken) {
+      this.lastLastSpoken[turnValue] = this.lastSpoken[turnValue];
+    }
+    this.lastSpoken[turnValue] = this.turn;
+    this.turns.push(turnValue);
+    this.mostRecentlySpoken = turnValue;
+    this.turn++;
+  }
+
+  playTurn() {
+    const turnValue = this.getAge(this.mostRecentlySpoken);
+    this.applyTurn(turnValue);
+  }
+
+  playUntil(endTurn) {
+    while (this.turn < endTurn) {
+      this.playTurn();
+      // this.print();
+    }
+    return this.mostRecentlySpoken;
+  }
+
+  print() {
+    console.log(this);
+  }
+}
+
+// const INPUT_ARRAY = [0, 3, 6];
+const INPUT_ARRAY = [5, 2, 8, 16, 18, 0, 1];
+// console.log(new Game(INPUT_ARRAY).playUntil(2020));
+console.log(new Game(INPUT_ARRAY).playUntil(30000000));
