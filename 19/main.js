4d1e84103e0973376892f45bc0a1494f15f29883
Lâm Nguyen Hoang <lamnguyenhoang@gmail.com>
2020-12-19 22:31:38 +0100

    day 19 part 2

--- 19/main.js
+++ 19/main.js
@@ -16,7 +16,7 @@ class Validator {
     this.rules = rules;
   }
 
-  validates(ruleId, message, index, checkLength = false) {
+  validates(ruleId, message, index, checkLength = false, depth = 0) {
     let i = index;
     const rule = this.rules.get(ruleId);
 
@@ -31,8 +31,13 @@ class Validator {
       for (const ruleSet of rule.needs) {
         let j = i;
         for (const subRuleId of ruleSet) {
-          const subRule = this.rules.get(subRuleId);
-          const subValidation = this.validates(subRuleId, message, j);
+          const subValidation = this.validates(
+            subRuleId,
+            message,
+            j,
+            false,
+            depth + 1
+          );
           if (subValidation.isValid) {
             j += subValidation.delta;
             lastValidation = true;
@@ -44,7 +49,11 @@ class Validator {
         }
         isValid = lastValidation;
         delta = isValid ? j - i : 0;
-        if (isValid) break;
+        if (isValid) {
+          const empty = new Array(depth).fill('    ').join('');
+          // console.log(empty, rule.id, `(${depth})`, ruleSet);
+          break;
+        }
       }
     }
 
@@ -53,6 +62,48 @@ class Validator {
     }
 
     return { isValid, delta };
+  }
+
+  clean(message, ruleId) {
+    const unwanted = this.rules.get(ruleId).needs;
+    let lastValidation = false;
+    let delta = 0;
+    let isValid = true;
+    let newMessage = message.slice();
+    let numFound = 0;
+
+    while (isValid) {
+      let i = 0;
+      for (const ruleSet of unwanted) {
+        let j = i;
+        for (const subRuleId of ruleSet) {
+          const subValidation = this.validates(
+            subRuleId,
+            newMessage,
+            j,
+            false,
+            0
+          );
+          if (subValidation.isValid) {
+            j += subValidation.delta;
+            lastValidation = true;
+          } else {
+            // try next rule set
+            lastValidation = false;
+            break;
+          }
+        }
+        isValid = lastValidation;
+        if (isValid) {
+          numFound++;
+          // console.log('found ' + ruleSet);
+          delta = j - i;
+          newMessage = newMessage.slice(delta);
+          break;
+        }
+      }
+    }
+    return [newMessage, numFound];
   }
 }
 
@@ -69,13 +120,67 @@ const getInput = (fileName) => {
   return { validator, messages };
 };
 
-const INPUT_FILES = ['data.csv'];
-for (const inputFile of INPUT_FILES) {
-  const { validator, messages } = getInput(inputFile);
-  let numValid = 0;
-  for (const message of messages) {
-    const { isValid, delta } = validator.validates('0', message, 0, true);
-    if (isValid) numValid += 1;
-  }
-  console.log(inputFile + ': ' + numValid);
+const INPUT_FILE = 'data.csv';
+const { validator, messages } = getInput(INPUT_FILE);
+
+// part 1
+const validMessages = new Set();
+for (const message of messages) {
+  const { isValid, delta } = validator.validates('0', message, 0, true);
+  if (isValid) validMessages.add(message);
 }
+console.log('part 1: ' + validMessages.size);
+
+// part 2
+// For part 2, any message that starts with any number of 42*X, then ends with
+// any 31 and 42 in succession (ending with 31) is valid.
+let numValid = 0;
+for (const message of messages) {
+  // Any number of 42
+  const [newMessage42, numFound42] = validator.clean(message, '42');
+
+  // Not a positive if we only have 42s (we also need at least one 31)
+  if (newMessage42.length === message.length || newMessage42.length === 0)
+    continue;
+
+  // Any number of 31
+  const [newMessage31, numFound31] = validator.clean(newMessage42, '31');
+
+  if (numFound31 > 0 && numFound42 > numFound31 && newMessage31.length === 0) {
+    // console.log(message);
+    numValid++;
+  }
+
+  //
+  // // Check for a succession of 42s and 31s ending with a 31
+  // let newMessage2 = newMessage;
+  // let lastIs42 = false;
+  //
+  // while (true) {
+  //   let check1 = validator.clean(newMessage2, '31');
+  //   if (check1.length != newMessage2.length) {
+  //     lastIs42 = false;
+  //     newMessage2 = check1;
+  //     continue;
+  //   }
+  //
+  //   let check2 = validator.clean(newMessage2, '42');
+  //   if (check2.length != newMessage2.length) {
+  //     console.log(message, check2, newMessage2);
+  //     lastIs42 = true;
+  //     newMessage2 = check2;
+  //     continue;
+  //   }
+  //
+  //   // get out if nothing has changed
+  //   break;
+  // }
+  // if (newMessage2.length === 0 && lastIs42 === false) {
+  //   numValid++;
+  //   // console.log(message);
+  // }
+  // if (lastIs42) {
+  //   console.log('ended with 42: ', message);
+  // }
+}
+console.log('part 2: ' + numValid);
